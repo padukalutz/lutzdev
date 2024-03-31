fc2bf060e55fe9a0527efb45c8db162d9f6c60ef
RUS MANTO <157650320+padukalutz@users.noreply.github.com>
2024-03-31 14:07:55 +0700

    Lutz Dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,5 @@ sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 });
 sr.reveal(`.profile__buttons`, { delay: 800 });
 sr.reveal(`.filters__content`, { delay: 900 });
 sr.reveal(`.filters`, { delay: 1000 });
+
+console.log("Fuck You Bitch");
